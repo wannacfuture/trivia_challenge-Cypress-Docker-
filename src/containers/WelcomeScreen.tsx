fd1b5be4f7153fe1e3d3ef399c2638d682280b8f
HARALD ANDERSON <danddycandy@gmail.com>
2023-04-27 20:49:53 -0700

    added: WelcomeScreen with animation.

--- src/containers/WelcomeScreen.tsx
+++ src/containers/WelcomeScreen.tsx
@@ -1,14 +1,25 @@
 import React from "react";
 
+import "./style.scss";
+
 export interface WelcomeScreenProps {}
 
 export const WelcomeScreen: React.FC<WelcomeScreenProps> = () => {
   return (
     <>
-      <div className="flex max-w-full justify-center ">
-        <h2 className="animate-bounce  text-3xl text-white font-bold font-mono subpixel-antialiased">
+      <div className="flex max-w-full justify-center flex-col select-none">
+        <h2 className="relative animate-fromtop text-3xl text-white font-bold font-mono subpixel-antialiased">
           CAN YOU SCORE 100%?
         </h2>
+        <h3 className="relative animate-fromleft text-5xl text-green-400 font-bold font-mono subpixel-antialiased">
+          Welcome to the Trivia Challenge!
+        </h3>
+        <h4 className="relative animate-fromright text-4md text-gray-300 font-bold font-sans subpixel-antialiased">
+          You will be presented with 10 True or False questions.
+        </h4>
+        <div className=" flex  self-center animate-fade">
+          <div className="pulse project-view justify-center w-[150px]">GO!</div>
+        </div>
       </div>
     </>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,12 +4,27 @@ module.exports = {
   theme: {
     extend: {
       animation: {
-        "spin-slow": "spin 3s linear infinite",
+        fromtop: "fromtop 0.5s ease-out ",
+        fade: "fade 2s ease-in 1s backwards",
+        fromleft: "fromleft 0.3s ease-in 0.4s backwards ",
+        fromright: "fromright 0.3s ease-in 0.6s backwards ",
       },
       keyframes: {
-        scoreanimation: {
+        fade: {
           "0%": { opacity: 0 },
           "100%": { opacity: 100 },
+        },
+        fromtop: {
+          "0%": { top: -1200, opacity: 0 },
+          "100%": { top: 0 },
+        },
+        fromleft: {
+          "0%": { left: -1500, opacity: 0 },
+          "100%": { left: 0 },
+        },
+        fromright: {
+          "0%": { left: -1500, opacity: 0 },
+          "100%": { left: 0 },
         },
       },
     },
